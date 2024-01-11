62ae9b1badded0b5c76cb6488346bd0eb2e06d59
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2024-01-11 11:36:32 +0530

    Styling Changes x2

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,10 +1,10 @@
 module.exports = {
-	useTabs: true,
-	tabWidth: 2,
-	singleQuote: false,
-	trailingComma: "all",
-	printWidth: 80,
-	plugins: ["prettier-plugin-svelte", "prettier-plugin-tailwindcss"],
-	tailwindConfig: "./tailwind.config.js",
-	overrides: [{ files: "*.svelte", options: { parser: "svelte" } }],
+  useTabs: false,
+  tabWidth: 2,
+  singleQuote: false,
+  trailingComma: "all",
+  printWidth: 80,
+  plugins: ["prettier-plugin-svelte", "prettier-plugin-tailwindcss"],
+  tailwindConfig: "./tailwind.config.js",
+  overrides: [{ files: "*.svelte", options: { parser: "svelte" } }],
 };
